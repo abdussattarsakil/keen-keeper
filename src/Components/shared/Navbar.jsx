f57f5bd15c887c2696531899477540979582012e
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-05-16 18:06:24 +0600

    react icons install and check routes check

--- src/Components/shared/Navbar.jsx
+++ src/Components/shared/Navbar.jsx
@@ -1,8 +1,10 @@
+import { BiAccessibility } from "react-icons/bi";
 
 const Navbar = () => {
     return (
-        <div>
-            Footer
+        <div className="text-7xl btn btn-primary p-10">
+           <BiAccessibility />
+            NavBar
         </div>
     );
 };
