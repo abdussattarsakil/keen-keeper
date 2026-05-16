606a0ea9b68265a4d4c86ec8ee8d3d8cd7050b30
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-05-16 19:10:15 +0600

    footer add

--- src/Components/shared/Footer.jsx
+++ src/Components/shared/Footer.jsx
@@ -1,8 +1,29 @@
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className="bg-green-900 p-12">
+            <div className="text-center space-y-4">
+                <h1 className="text-6xl text-white font-bold">KeenKeeper</h1>
+                <p className="text-gray-300">
+                    Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.
+                </p>
+
+                <h4 className="text-xl text-gray-200">Social Links</h4>
+                <div className="flex justify-center gap-3">
+                    <img src="/assets/instagram.png" alt="instagram" />
+                    <img src="/assets/facebook.png" alt="facebook" />
+                    <img src="/assets/twitter.png" alt="twitter" />
+                </div>                
+            </div>
+            <div className="container mx-auto h-[px] border-t bg-gray-500 m-5"></div>
+            <div className="flex justify-around">
+                <p className="text-gray-400">© 2026 KeenKeeper. All rights reserved.</p>
+                <div className="flex gap-5 text-gray-400">
+                    <p>Privacy Policy</p>
+                    <p>Terms of Service</p>
+                    <p>Cookies</p>
+                </div>
+            </div>
         </div>
     );
 };
