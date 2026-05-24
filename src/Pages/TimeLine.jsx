b9ca373a44fdd6d9320250430489b62818af1004
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-05-25 05:27:45 +0600

    call text video timeline show

--- src/Pages/TimeLine.jsx
+++ src/Pages/TimeLine.jsx
@@ -1,14 +1,40 @@
 import { useContext } from "react";
 import { TimelineContext } from "../Components/context/TimelineContext";
 
+import callImg from "/assets/call.png"
+import videoImg from "/assets/video.png"
+import textImg from "/assets/text.png"
+
+
+const actionImages = {
+    Call: callImg,
+    Video: videoImg,
+    Text: textImg
+};
+
+
 const TimeLine = () => {
 
-    const {name, type}=useContext(TimelineContext);
-    console.log(name, type);
-    
+    const { TimelineUpdate } = useContext(TimelineContext);
+    console.log(TimelineUpdate);
+
+
+
     return (
         <div>
-            <h1>{name}</h1>
+            {
+                TimelineUpdate.map(data => (
+                    <div className="container mx-auto m-10 p-10 shadow-2xl flex gap-5">
+                        <img src={actionImages[data.action]} alt="" />
+                        <div>
+                            <h1>{data.action} <span>with {data.name}</span></h1>
+                            <small>
+                                {new Date(data.time).toLocaleString()}
+                            </small>
+                        </div>
+                    </div>
+                ))
+            }
         </div>
     );
 };
