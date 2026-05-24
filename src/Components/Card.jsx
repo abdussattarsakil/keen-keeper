eec6180d8b85cf6f5dae731f12648cc98e224bda
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-05-25 02:18:01 +0600

    all card dynamic status design add

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,29 +3,36 @@ import { Link } from "react-router";
 const Card = ({ data }) => {
     // console.log(data);
     return (
-        <div class="hover-3d shadow-sm rounded-2xl">
+        <div className="transition-transform duration-3000 ease-in-out hover:scale-120">
+            <Link to={`profileDetails/${data.id}`}>
+                <div className="shadow-2xl rounded-2xl text-center py-7 cursor-pointer">
+                    <img className="w-25 rounded-full mx-auto" src={data.picture} alt={data.name} />
 
-            <figure class="max-w-100">
-                <Link to={`profileDetails/${data.id}`}>
-                    <div className=" text-center py-7">
-                        <img className="w-25 rounded-full mx-auto" src={data.picture} alt={data.name} />
-                        <h1>{data.name}</h1>
-                        <p>{data.days_since_contact}</p>
-                        <h2>{data.tags}</h2>
-                        <h2>{data.status}</h2>
-                    </div>
-                </Link>
-            </figure>
+                    <h1 className="text-xl font-semibold">{data.name}</h1>
 
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+                    <p className="text-sm text-gray-400">{data.days_since_contact}d ago</p>
+
+
+                    {
+                        data.tags.map(tag => (
+                            <div className="mt-2">
+                                <span className={`bg-green-100 text-xs px-3 py-1 rounded-full`}>
+                                    {tag}
+                                </span>
+                            </div>
+                        ))
+                    }
+
+
+                    <h2 className={`
+                        ${data.status == "on-track" && "text-green-500"}
+                        ${data.status == "overdue" && "text-red-500"}
+                        ${data.status == "almost due" && "text-yellow-500"}                        
+                        `}>{data.status}</h2>
+                </div>
+            </Link>
         </div>
+
     );
 };
 
