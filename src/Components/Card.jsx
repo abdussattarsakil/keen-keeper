a461c1968a4aaf82a71a4e8e81200176117248b8
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-05-25 01:30:20 +0600

    daisy ui hover 3d add

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,18 +3,29 @@ import { Link } from "react-router";
 const Card = ({ data }) => {
     // console.log(data);
     return (
-        <div className="">
-            <Link to={`profileDetails/${data.id}`}>
-                <div className="shadow-sm rounded-2xl text-center py-7">
-                    <img className="w-25 rounded-full mx-auto" src={data.picture} alt={data.name} />
-                    <h1>{data.name}</h1>
-                    <p>{data.days_since_contact}</p>
-                    <h2>{data.tags}</h2>
-                    <h2>{data.status}</h2>
-                </div>
-            </Link>
-        </div>
+        <div class="hover-3d shadow-sm rounded-2xl">
 
+            <figure class="max-w-100">
+                <Link to={`profileDetails/${data.id}`}>
+                    <div className=" text-center py-7">
+                        <img className="w-25 rounded-full mx-auto" src={data.picture} alt={data.name} />
+                        <h1>{data.name}</h1>
+                        <p>{data.days_since_contact}</p>
+                        <h2>{data.tags}</h2>
+                        <h2>{data.status}</h2>
+                    </div>
+                </Link>
+            </figure>
+
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
     );
 };
 
