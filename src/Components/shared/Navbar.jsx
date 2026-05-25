c921d90eb21a37970dfa9c980b2cdfd78288553c
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-05-25 09:11:45 +0600

    navbar mobile dropdown menu fix

--- src/Components/shared/Navbar.jsx
+++ src/Components/shared/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
             </div>
 
             {/* mobile dropdown */}
-            <div className="dropdown dropdown-hover md:hidden">
+            <div className="dropdown dropdown-end md:hidden">
                 <div tabIndex={0} role="button" className="btn m-1">Menubar</div>
                 <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
                     <li>
