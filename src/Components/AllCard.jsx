edec1163ecf118734fae2169d76742fd3be82f88
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-05-25 09:02:59 +0600

    home page ui fix

--- src/Components/AllCard.jsx
+++ src/Components/AllCard.jsx
@@ -1,25 +1,27 @@
 import { use } from "react";
 import Card from "./Card";
 
-const dataPromise =(async()=>{
+const dataPromise = (async () => {
     const res = await fetch("/data.json");
     const data = await res.json();
 
-    await new Promise (resolve=> setTimeout(resolve,1000));
+    await new Promise(resolve => setTimeout(resolve, 1000));
 
     return data
 })();
 const AllCard = () => {
     const allData = use(dataPromise);
-    console.log(allData,"allCard async data fetch");
-    return (
-        <div className="container mx-auto grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+    console.log(allData, "allCard async data fetch");
+    return (<>
+    <h1 className="text-3xl font-bold container mx-auto">Your Friends</h1>
+        <div className="container mx-auto grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 my-10">
             {
-                allData.map(data=>(
+                allData.map(data => (
                     <Card data={data}></Card>
                 ))
             }
         </div>
+    </>
     );
 };
 
